aa51203ccf3fdab7af4ad6da93b159391c9d2f5a
Nurahmet <n.azhibek@astanait.edu.kz>
2020-10-13 16:22:19 +0500

    Login and Logout without DB

---
+++ src/main/java/Servlets/LoginServlet.java
@@ -0,0 +1,27 @@
+package Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "Servlets.LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        HttpSession session = request.getSession();
+        session.setAttribute("login", login);
+        request.getRequestDispatcher("home.jsp").forward(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
